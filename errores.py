911f66f5445f098c495ec568e36cec53df64f5df
Jesus Figuera <figueraj196@gmail.com>
2022-10-21 13:45:22 -0400

    Agregadas funciones para el calculo del error y teoremas.

---
+++ errores.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+def error_absoluto(valor_real : float, valor_aproximado : float) -> float:
+
+    """
+    
+    Calcula el error absoluto. Mediante una resta del valor real y  el valor aproximado
+    devolviendo el valor absoluto de esa resta.
+    
+    
+    return : float
+    """
+
+
+    return abs(valor_real - valor_aproximado)
+
+
+
+def error_relativo(error_absoluto: float,valor_real : float) ->  float:
+
+    """
+    
+    Calcula el error relativo, mediante una division del error absoluto
+    entre el valor real, retornando el valor absoluto de esa division.
+
+    return: float
+    
+    """
+
+
+    return abs(error_absoluto/valor_real)
+
+
+
+
+if __name__ == "__main__":
+
+    #caso 1
+
+    valor_real = 20_000
+    valor_aproximado = 19_999 
+    err_abs = error_absoluto(valor_real,valor_aproximado)
+    err_re = error_relativo(err_abs,valor_real)
+    print(f"Error absoluto: {err_abs}")
+    print(f"Error relativo: {err_re}")
+
+    #caso 2
+
+    valor_real = 5
+    valor_aproximado = 4
+
+    err_abs = error_absoluto(valor_real,valor_aproximado)
+    err_re = error_relativo(err_abs,valor_real)
+
+    print(f"Erro absoluto: {err_abs}")
+    print(f"Erro relativo: {err_re}")
---
+++ teoremas.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+
+
+def existe_valor_intermedio(f, x: float, y: float) -> bool:
+
+    """
+    Se toma una funcion matematica cualquiera
+    i.e: x^2 - 4 continua
+    y  un intervalo (x,y).
+    donde si el producto, de evaluar ambos valores en esa funcion
+    es < 0 existe un valor p  en el intervalo, tal que f(p) es = 0.
+
+    """
+
+    return (f(x) * f(y)) < 0    
+
+
+
+
+def es_numero_raiz(f,x):
+
+    """
+    Se evalua si el valor dado 
+    evaluado en la funcion
+    es raiz de de la funcion.
+    
+    """
+
+    return f(x) == 0
+
+
+if __name__ == "__main__":
+
+    f = lambda x: x**2 - 4
+
+    a = 0
+    b = 5
+
+
+
+    #casos de prueba.    
+    print(f"Existe valor intermedio: {existe_valor_intermedio(f,a,b)}")
+
+
+
+    print(f"Es numero raiz: {es_numero_raiz(f,2)}")
+    print(f"Es numero raiz: {es_numero_raiz(f,-2)}")
+    print(f"Es numero raiz: {es_numero_raiz(f,4)}")
+    print(f"Es numero raiz: {es_numero_raiz(f,4)}")
